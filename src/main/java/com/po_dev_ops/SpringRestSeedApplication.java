71faa15eb728d746d9f68d55bdc4096a40850f76
Peter <ophalsp@users.noreply.github.com>
2017-08-08 20:08:20 +0200

    Add Spring Boot REST Service

---
+++ src/main/java/com/po_dev_ops/SpringRestSeedApplication.java
@@ -0,0 +1,12 @@
+package com.po_dev_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSeedApplication.class, args);
+    }
+}
